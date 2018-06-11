ca10e26aa833404d0f22f410d19363a9230d081f
pawelzwronek <pawel.z.wronek@gmail.com>
2018-06-11 14:47:44 +0200

    F: add-in name

--- make-bundle.cmd
+++ make-bundle.cmd
@@ -12,9 +12,9 @@ copy pyHook\_cpyHook.cp35-win_amd64.pyd  %dst%\pyHook
 copy pyHook\cpyHook.py  %dst%\pyHook
 copy pyHook\HookManager.py  %dst%\pyHook
 copy pyHook\LICENSE.txt  %dst%\pyHook
-copy NoGestures.manifest  %dst%
+copy NoGesturesNoShift.manifest  %dst%
 copy autohotkey.py  %dst%
-copy NoGestures.py  %dst%
+copy NoGesturesNoShift.py  %dst%
 copy icons\NoGestures.png %dst%\icons
 
 pause
